7ef0ca0a9f02220baf5f892fb2676efa8a796c2e
ahrian87 <maciej.zbydniewski@gmail.com>
2021-10-17 16:04:07 +0200

    Zmiana list na automatycznie generowanie przez python - w zakresie oraz przez iterację innego zbioru. Dodanie zakresu wartości dla każdej osi, zdefiniowanie koloru punktów funkcji scatter

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use("seaborn")
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, c="red", s=10)
 
 # Zdefiniowanie tytułu wykresu i etykiet osi.
 ax.set_title("Kwadraty liczb", fontsize=24)
@@ -14,5 +14,8 @@ ax.set_ylabel("Kwadraty wartości", fontsize=14)
 
 # Zdefiniowanie wielkości etykiet.
 ax.tick_params(axis="both", which="major", labelsize=14)
+
+# Zdefiniowanie zakresu dla każdej osi
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
