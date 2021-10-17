e36cb36ca91a56227c9f85f6dcff7fdfab9f001e
ahrian87 <maciej.zbydniewski@gmail.com>
2021-10-17 15:55:45 +0200

    Dodanie wykresu celem pokazania kilku punktów za pomocą funkcji scatter - punkty od 1 do 5 i zaznaczone ich kwadraty - od 1 do 25

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,7 +1,18 @@
 import matplotlib.pyplot as plt
 
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
 plt.style.use("seaborn")
 fig, ax = plt.subplots()
-ax.scatter(2, 4)
+ax.scatter(x_values, y_values, s=100)
+
+# Zdefiniowanie tytułu wykresu i etykiet osi.
+ax.set_title("Kwadraty liczb", fontsize=24)
+ax.set_xlabel("Wartość", fontsize=14)
+ax.set_ylabel("Kwadraty wartości", fontsize=14)
+
+# Zdefiniowanie wielkości etykiet.
+ax.tick_params(axis="both", which="major", labelsize=14)
 
 plt.show()
