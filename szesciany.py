8f3cee82523d1374ae4fe6e71fdaa780aaff7fe7
ahrian87 <maciej.zbydniewski@gmail.com>
2021-10-18 09:19:52 +0200

    Wykres punktowy dla sześcianów liczb, zakres 1-5000, kolorowy wykres - gradient Accent, obrzydliwy

--- szesciany.py
+++ szesciany.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
-x_values = range(1, 6)
+x_values = range(1, 5001)
 y_values = [x**3 for x in x_values]
 
 plt.style.use('Solarize_Light2')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, c="blue", s=50)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Accent, s=10)
 
 # Zdefiniowanie tytułu wykresu i etykiet osi
 ax.set_title("Sześciany liczb", fontsize=24)
@@ -16,6 +16,6 @@ ax.set_ylabel("Sześciany wartości", fontsize=14)
 ax.tick_params(axis="both", labelsize=14)
 
 # Zdefiniowanie zakresu dla każdej osi.
-ax.axis([0, 6, 0, 150])
+ax.axis([0, 5555, 0, 150000000000])
 
 plt.show()
